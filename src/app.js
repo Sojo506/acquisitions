3046addbd834b2b9865fd2d2ca11a8b9aafe51e8
Sojo506 <fsojodev@gmail.com>
2026-05-12 07:42:23 -0600

    initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello, from Acquisitions!' });
+});
+
+export default app;
